7b3d1017aecfed882f5ac1bd2c4c25e59334bcef
Loubejac-Combalbert Jean-Philippe <loubejac-com@insa-toulouse.fr>
2024-11-22 16:57:49 +0100

    refactor: clone_nodes and gmap

--- src/tools.ml
+++ src/tools.ml
@@ -2,10 +2,8 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes (gr:'a graph) = n_fold gr (fun grb node -> new_node grb node) empty_graph
-let gmap gr f = let newGr = clone_nodes gr 
-    in
-    e_fold gr (fun grb arc -> new_arc grb {src=arc.src; tgt=arc.tgt;lbl=f arc.lbl}) newGr
+let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph
+let gmap gr f = e_fold gr (fun grb arc -> new_arc grb {src=arc.src; tgt=arc.tgt;lbl=f arc.lbl}) (clone_nodes gr)
     
 (*Even if the specified prototype on website is with int lbl, we chose 
   to implement the function in order to allow a polymorphic lbl*)
