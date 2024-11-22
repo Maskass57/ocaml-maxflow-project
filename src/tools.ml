ee8ab56163aeddab920c018b562712f6121ba62f
Loubejac-Combalbert Jean-Philippe <loubejac-com@insa-11268>
2024-11-22 16:41:54 +0100

    feat: implement clone_nodes, gmap and add_arc

--- src/tools.ml
+++ src/tools.ml
@@ -2,6 +2,11 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes _gr = assert false
-let gmap _gr _f = assert false
-(* Replace _gr and _f by gr and f when you start writing the real function. *)
+let clone_nodes (gr:'a graph) = n_fold gr (fun grb node -> new_node grb node) empty_graph
+let gmap gr f = let newGr = clone_nodes gr 
+    in
+    e_fold gr (fun grb arc -> new_arc grb {src=arc.src; tgt=arc.tgt;lbl=f arc.lbl}) newGr
+    
+(*Even if the specified prototype on website is with int lbl, we chose 
+  to implement the function in order to allow a polymorphic lbl*)
+let  add_arc gr src tgt lbl = new_arc gr {src=src; tgt=tgt;lbl=lbl}
