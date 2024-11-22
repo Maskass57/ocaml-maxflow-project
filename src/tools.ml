b6345faf58923e147509499ddd113c200886376e
Clement Lacau <lacau@insa-toulouse.fr>
2024-11-22 17:04:48 +0100

    refactor: add_arcs

--- src/tools.ml
+++ src/tools.ml
@@ -7,4 +7,7 @@ let gmap gr f = e_fold gr (fun grb arc -> new_arc grb {src=arc.src; tgt=arc.tgt;
     
 (*Even if the specified prototype on website is with int lbl, we chose 
   to implement the function in order to allow a polymorphic lbl*)
-let  add_arc gr src tgt lbl = new_arc gr {src=src; tgt=tgt;lbl=lbl}
+let  add_arc gr src tgt lbl = 
+  match (find_arc gr src tgt) with
+    | None -> new_arc gr {src=src; tgt=tgt;lbl=lbl}
+    | Some {src = a; tgt = b; lbl = nlbl} -> new_arc gr {src = a ; tgt = b ; lbl = lbl + nlbl}
