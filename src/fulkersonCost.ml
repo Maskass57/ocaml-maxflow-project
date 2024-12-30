29831460557929036a31e4621adba7f1dba842db
Clement Lacau <lacau@insa-toulouse.fr>
2024-12-30 02:44:22 +0100

    make format

--- src/fulkersonCost.ml
+++ src/fulkersonCost.ml
@@ -78,19 +78,19 @@ let get_node = function
   | Some x -> x
   | None -> failwith "aargh.."
 
-  let convert_dijkstra_to_fulkerson (node, dijkstra_label) =
-{ flow = 0; (* Flow is not part of Dijkstra's algorithm, so it's initialized to 0 *)
-  capa = 0; (* Capacity is also not part of Dijkstra, so initialized to 0 *)
-  cost = dijkstra_label.cost }  (* The cost in the Dijkstra label becomes the cost in the Fulkerson label *)
+let convert_dijkstra_to_fulkerson (node, dijkstra_label) =
+  { flow = 0; (* Flow is not part of Dijkstra's algorithm, so it's initialized to 0 *)
+    capa = 0; (* Capacity is also not part of Dijkstra, so initialized to 0 *)
+    cost = dijkstra_label.cost }  (* The cost in the Dijkstra label becomes the cost in the Fulkerson label *)
 let convert_dijkstra_graph_to_fulkerson_graph (dijkstra_graph: 'a dijkstra_label graph) : fulkerson_graphs_cost =
-    gmap dijkstra_graph (fun dijkstra_label ->
+  gmap dijkstra_graph (fun dijkstra_label ->
       (* Convert dijkstra_label to fulkerson_label_cost *)
       { flow = 0;       (* No flow in Dijkstra, so initialize to 0 *)
         capa = 0;       (* Capacity is unknown, so set to 0 *)
         cost = dijkstra_label.cost }  (* The cost from Dijkstra is directly used *)
     )
 
-  let dijkstra graph start target = 
+let dijkstra graph start target = 
   let rec dijkstra_aux list visited = 
     let current_node = find_min_node list in 
     match (current_node) with
@@ -125,15 +125,15 @@ let rec find_min_flow graph minimum (dijkstra_graph: int dijkstra_graph)= match 
     find_min_flow graph new_minimum ((node_aux,label_aux)::rest)
 
 let rec updateEdgeGraph_cost gr (dijkstraResult:'a dijkstra_graph) min_flow = 
-    (*let min_flow = find_min_flow gr max_int dijkstraResult in*)
-    match dijkstraResult with
-    | (nodex,_)::(nodey,labely)::rest -> 
-        let arc = List.find (fun {src=src;tgt=tgt;lbl=_} -> src = nodex && tgt = nodey ) (out_arcs gr nodex) in
-        let gr1 = add_arc_cost gr nodex nodey ({flow = (-min_flow);cost=arc.lbl.cost}) in 
-        let gr2 = add_arc_cost gr1 nodex nodey ({flow = (min_flow);cost=(-arc.lbl.cost)}) in
-        updateEdgeGraph_cost gr2 (((nodey,labely)::rest)) min_flow
-    | _x::[] -> gr
-    | [] -> gr
+  (*let min_flow = find_min_flow gr max_int dijkstraResult in*)
+  match dijkstraResult with
+  | (nodex,_)::(nodey,labely)::rest -> 
+    let arc = List.find (fun {src=src;tgt=tgt;lbl=_} -> src = nodex && tgt = nodey ) (out_arcs gr nodex) in
+    let gr1 = add_arc_cost gr nodex nodey ({flow = (-min_flow);cost=arc.lbl.cost}) in 
+    let gr2 = add_arc_cost gr1 nodex nodey ({flow = (min_flow);cost=(-arc.lbl.cost)}) in
+    updateEdgeGraph_cost gr2 (((nodey,labely)::rest)) min_flow
+  | _x::[] -> gr
+  | [] -> gr
 
 let getVal_cost = function
   | Some x -> x
@@ -164,35 +164,35 @@ let dijkstra_list_to_graph (dijkstra_result: node dijkstra_graph) : node dijkstr
   (* Add edges corresponding to the predecessor links in the dijkstra_result *)
   List.fold_left
     (fun acc (node, { prev; cost }) ->
-      match prev with
-      | Some prev_node ->
-        let arc = { src = prev_node; tgt = node; lbl = { cost = cost; prev = Some prev_node } } in
-        new_arc acc arc  (* Add the constructed arc to the graph *)
-      | None -> acc  (* No edge if there's no predecessor *)
+       match prev with
+       | Some prev_node ->
+         let arc = { src = prev_node; tgt = node; lbl = { cost = cost; prev = Some prev_node } } in
+         new_arc acc arc  (* Add the constructed arc to the graph *)
+       | None -> acc  (* No edge if there's no predecessor *)
     )
     graph_with_nodes
     dijkstra_result
 
-        let print_dijkstra_path (path: node dijkstra_graph) =
-          let formatted_path =
-            String.concat " -> " (List.map (fun (node, _) -> string_of_int node) path)
-          in
-          Printf.printf "Path found: [%s]\n" formatted_path
+let print_dijkstra_path (path: node dijkstra_graph) =
+  let formatted_path =
+    String.concat " -> " (List.map (fun (node, _) -> string_of_int node) path)
+  in
+  Printf.printf "Path found: [%s]\n" formatted_path
 (* Convert edgegraph_label to fulkerson_label_cost by adding a default capa *)
 let convert_edgegraph_to_fulkerson_label (edge_label: edgegraph_label) : fulkerson_label_cost =
   { flow = edge_label.flow; cost = edge_label.cost; capa = 0 }  (* You can set capa based on your logic *)
 
-  let convert_edgegraph_graph_to_fulkerson (gr: edgegraph_label graph) : fulkerson_graphs_cost =
-    gmap gr (fun { flow; cost } -> { flow; cost; capa = 0 })  (* Convert each edge *)
-  
- (* Function to convert fulkerson_label_cost to edgegraph_label *)
+let convert_edgegraph_graph_to_fulkerson (gr: edgegraph_label graph) : fulkerson_graphs_cost =
+  gmap gr (fun { flow; cost } -> { flow; cost; capa = 0 })  (* Convert each edge *)
+
+(* Function to convert fulkerson_label_cost to edgegraph_label *)
 let convert_fulkerson_to_edgegraph_label (f: fulkerson_label_cost) : edgegraph_label =
   { flow = f.flow; cost = f.cost }  (* Ignore capa field *)
 
 (* Function to convert a full fulkerson_graphs_cost to edgegraph_label graph *)
 let convert_fulkerson_graph_to_edgegraph (gr: fulkerson_graphs_cost) : edgegraph_label graph =
   gmap gr (fun { flow; cost; _ } -> { flow; cost })  (* Ignore capa field *)
-           
+
 let fordFulkerson gr origin destination =
   let fulkerson = convertGraph_Cost gr in 
   let eGraph = edgeGraph_cost fulkerson in
@@ -213,10 +213,9 @@ let fordFulkerson gr origin destination =
       let edged = edgeGraph_cost temp in
       if(list=[]) then gr1 
       else
-        
+
         let min_flow = find_min_flow edged max_int list in 
         fordFulkersonAux (convert_edgegraph_graph_to_fulkerson (updateEdgeGraph_cost edged start_graph min_flow)) (i+1) 
   in
   unEdgeGraph_cost fulkerson (convert_fulkerson_graph_to_edgegraph (fordFulkersonAux fulkerson 0))
-      
-      
+
