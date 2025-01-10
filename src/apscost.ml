343d60b4dacc0a6a151e4180644a40ab9cb9f3dd
Clement Lacau <lacau@insa-toulouse.fr>
2025-01-10 03:59:46 +0100

    chore: format

--- src/apscost.ml
+++ src/apscost.ml
@@ -69,8 +69,8 @@ let add_origin_destination_cost graph nbreSportsChoisis =
         let updated_graph = 
           if (id >= 1 && id < 100) then 
             new_arc current_graph {src=1000;tgt=id;lbl={capa=nbreSportsChoisis;cost=0}}
-          (*else if (id > 100 && id <1000) then
-            new_arc current_graph {src=id;tgt=1001;lbl={capa=1;cost=0}}*)
+            (*else if (id > 100 && id <1000) then
+              new_arc current_graph {src=id;tgt=1001;lbl={capa=1;cost=0}}*)
           else 
             current_graph 
         in
