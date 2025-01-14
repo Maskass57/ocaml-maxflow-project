b5964dd5d786a3f5bb21cae79ed133ed157b038d
Loubejac-Combalbert Jean-Philippe <loubejac-com@insa-toulouse.fr>
2025-01-14 17:53:48 +0100

    refactor: remove useless proto

--- src/apscost.mli
+++ src/apscost.mli
@@ -1,6 +1,5 @@
 open Graph
 open FulkersonCost
 
-val read_arc_cost: input_label graph -> string -> input_label graph
 val from_file_aps_cost: string -> input_label graph
 val add_origin_destination_cost: input_label graph -> id -> input_label graph
