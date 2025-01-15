64e16adea2eb093600ae1241952dbefdaec4fe33
Clement Lacau <lacau@insa-toulouse.fr>
2025-01-15 01:53:57 +0100

    add: some general infos: total cost, attribution of each sport

--- src/gui.ml
+++ src/gui.ml
@@ -51,15 +51,31 @@ let obtain_peopleID_sportID (gr:fulkerson_label_cost graph) =
 
 let obtain_name id hashtbl =
     Hashtbl.find hashtbl id
- 
+
+let get_first_val = function 
+  | [] -> (0,0)
+  | (flow,capa)::_ -> (flow,capa)
+
+let obtain_sport_flow_capa (gr:fulkerson_label_cost graph) id = 
+  let list = e_fold gr (fun acu arc -> 
+    if (arc.src = id && arc.tgt = 1001) then
+      (arc.lbl.flow,arc.lbl.capa)::acu
+    else 
+      acu) [] in 
+    get_first_val list
+
+let print_flow_capa (flow,capa) =
+  string_of_int flow ^ "/" ^string_of_int capa 
 
 (* Displays a textual result in a window *)
 let gui_display gr =  
   let path = infile in
     let _ = GMain.init () in
   let window = GWindow.window ~title:"Résultat Ford-Fulkerson" ~border_width:10 ~width:600 ~height:400 () in
-    let vbox = GPack.vbox ~packing:window#add () in
-    let button = GButton.button ~label:"Fermer" ~packing:vbox#pack () in
+    let hbox = GPack.hbox ~packing:window#add () in
+    let vbox_left = GPack.vbox ~packing:hbox#add () in
+    
+    let button = GButton.button ~label:"Fermer" ~packing:vbox_left#pack () in
     button#connect#clicked ~callback:Main.quit |> ignore;
     
     let hashtbl = find_name_id path in
@@ -67,8 +83,20 @@ let gui_display gr =
 
     List.iter(fun (idName,idSport) -> 
       let text_to_display = obtain_name idName hashtbl  ^ " a été attribué au sport : "  ^ obtain_name idSport hashtbl  in 
-      let text_view = GText.view ~packing:vbox#pack () in
+      let text_view = GText.view ~packing:vbox_left#pack () in
       text_view#buffer#set_text text_to_display) ids;
+    
+    let vbox_right = GPack.vbox ~packing:hbox#pack ~width:200 ~spacing:30 () in
+    let _label1 = GMisc.label ~text:("Cout total : " ^ string_of_int (getCostGraph gr)) ~packing:vbox_right#pack () in
+    let listeSports = List.fold_left (fun acu (_,idSport) -> 
+      let name_sport = Hashtbl.find hashtbl idSport in 
+      if (List.mem (name_sport,idSport) acu) then
+        acu
+      else 
+        (name_sport,idSport)::acu
+    ) [] ids in
+    let _label2 = GMisc.label ~text:("Résultat des sports: \n"
+    ^ List.fold_left (fun acu (nomSport,idSport) -> acu ^ "\n" ^ nomSport ^ " " ^ print_flow_capa (obtain_sport_flow_capa gr idSport)) "" listeSports) ~packing:vbox_right#pack () in
     
     window#show ();
     GMain.main ();
